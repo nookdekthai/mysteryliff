de34636bb2ac05b843a24b54f700f5bcd5e94b94
21 Tech Enterprise <21techenterprise@192.168.2.33>
2022-09-27 14:24:23 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
   }
 
   const initLine = () => {
-    liff.init({ liffId: '1655665373-YAopzeO6' }, () => {
+    liff.init({ liffId: '1657507530-V4xROv9X' }, () => {
       if (liff.isLoggedIn()) {
         runApp();
       } else {
